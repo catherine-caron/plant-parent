249ae5c9bbc23ed663e7916f9ee82b8a222c7e08
rajaabk <rajaa.bk0@gmail.com>
2022-01-06 15:29:09 -0500

    Changed Configs

---
+++ PlantCareApplication/PlantCareApplication-Backend/src/test/java/ca/plantcare/EventRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.plantcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
