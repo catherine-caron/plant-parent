8be5ba97f1a812ce4281317b5e54e9681269225a
rajaabk <rajaa.bk0@gmail.com>
2022-02-01 23:43:31 -0500

    Increase Plant Tests

--- PlantCareApplication-Backend/src/main/java/ca/plantcare/service/PlantService.java
+++ PlantCareApplication-Backend/src/main/java/ca/plantcare/service/PlantService.java
@@ -21,60 +21,61 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class PlantService {
 
-    @Autowired
+	@Autowired
 	private MemberRepository memberRepository;
 	@Autowired
 	private PlantRepository plantRepository;
 
-
 	/**
-     * Find all plants owned by specific member
+	 * Find all plants owned by specific member
+	 * 
 	 * @param memberUsername
 	 * @return
 	 */
 	@Transactional
-	public List<Plant> getPlantsByMember(String memberUsername){
+	public List<Plant> getPlantsByMember(String memberUsername) {
 		Member member = memberRepository.findMemberByUsername(memberUsername);
-		if (memberUsername == null){
+		if (memberUsername == null) {
 			throw new IllegalArgumentException("Member not found.");
-		}
-		else{
+		} else {
 			return member.getPlant();
 		}
 	}
-	
+
 	@Transactional
 	public Plant getPlantByBotanicalName(String botanicalName) {
-		if (botanicalName == null){
+		if (botanicalName == null) {
 			throw new IllegalArgumentException("Plant not found.");
 		}
 		Plant plant = plantRepository.findPlantByBotanicalName(botanicalName);
 		return plant;
-		
+
 	}
-	
+
 	@Transactional
 	public Plant getPlantByCommonName(String commonName) {
-		if (commonName == null){
+		if (commonName == null) {
 			throw new IllegalArgumentException("Plant not found.");
 		}
 		Plant plant = plantRepository.findPlantByCommonName(commonName);
 		return plant;
-		
+
 	}
-	
-/*	@Transactional
-	public List<Plant> getPlantBySunExposure(String memberUsername){
-		return null;
-	}*/
-	
+
+	/*
+	 * @Transactional public List<Plant> getPlantBySunExposure(String
+	 * memberUsername){ return null; }
+	 */
+
 	/**
-     * Find specific plant by its id. Admin function needed to assign right id to plants
+	 * Find specific plant by its id. Admin function needed to assign right id to
+	 * plants
+	 * 
 	 * @param plantId
 	 * @return plant
 	 */
 	@Transactional
-	private Plant getPlantByPlantId(Integer plantId){
+	private Plant getPlantByPlantId(Integer plantId) {
 		if (plantId == null || plantId < 0) {
 			throw new IllegalArgumentException("Id must not be null.");
 		}
@@ -83,48 +84,51 @@ public class PlantService {
 			throw new IllegalArgumentException("Cannot find plant with id " + plantId + " in repository.");
 		}
 		return plant;
-		
+
 	}
-	
-	//create plant by admin needs an admin member id
-	public Plant createPlant(Integer icon, String givenName, String botanicalName,
-			String commonName,SunExposure sunExposure,SoilType soilType,
-			Toxicity toxicity, List <BloomTime> bloomTime, WateringSchedule wateringRecommendation){
-		
-		if (icon == null ||  icon.equals("undefined")) {
-            throw new IllegalArgumentException("Icon cannot be null or empty");
-        }
-		
-		if (givenName == null || givenName == "" ) {
-            throw new IllegalArgumentException("Given Name cannot be null or empty");
+
+	// create plant by admin needs an admin member id
+	public Plant createPlant(Integer icon, String givenName, String botanicalName, String commonName,
+			SunExposure sunExposure, SoilType soilType, Toxicity toxicity, List<BloomTime> bloomTime,
+			WateringSchedule wateringRecommendation) {
+
+		if (icon == null || icon.equals("undefined")) {
+			throw new IllegalArgumentException("Icon cannot be null or empty");
 		}
-		
+
+		if (givenName == null || givenName == "") {
+			throw new IllegalArgumentException("Given Name cannot be null or empty");
+		}
+
 		if (botanicalName == null || botanicalName == "") {
-	            throw new IllegalArgumentException("Botanical Name cannot be null or empty");
-	    }
-		
-		if (commonName == null || commonName == "" ) {
-            throw new IllegalArgumentException("Common Name cannot be null or empty");
+			throw new IllegalArgumentException("Botanical Name cannot be null or empty");
 		}
-		if (sunExposure == null ) {
-            throw new IllegalArgumentException("Sun Exposure cannot be null or empty");
+
+		if (commonName == null || commonName == "") {
+			throw new IllegalArgumentException("Common Name cannot be null or empty");
 		}
-		if (toxicity == null ) {
-            throw new IllegalArgumentException("Toxicity cannot be null or empty");
+		if (sunExposure == null) {
+			throw new IllegalArgumentException("Sun Exposure cannot be null or empty");
 		}
-		if (bloomTime == null ) {
-            throw new IllegalArgumentException("Bloom Time cannot be null or empty");
+		if (toxicity == null) {
+			throw new IllegalArgumentException("Toxicity cannot be null or empty");
 		}
-		if (soilType == null ) {
-            throw new IllegalArgumentException("Soil Type cannot be null or empty");
+		if (bloomTime == null) {
+			throw new IllegalArgumentException("Bloom Time cannot be null or empty");
 		}
-		if (wateringRecommendation == null ) {
-            throw new IllegalArgumentException("Watering Recommendation cannot be null or empty");
-		} //need expection for wrong inputs
-		
+		if (soilType == null) {
+			throw new IllegalArgumentException("Soil Type cannot be null or empty");
+		}
+		if (wateringRecommendation == null) {
+			throw new IllegalArgumentException("Watering Recommendation cannot be null or empty");
+		} // need expection for wrong inputs
+
 		int leftLimit = 000001;
 		int rightLimit = 000100;
-	    int plantId = leftLimit + (int) (Math.random() * (rightLimit - leftLimit));
+		int plantId = leftLimit + (int) (Math.random() * (rightLimit - leftLimit));
+		if (plantId > 100) {
+			throw new IllegalArgumentException("PlantId is too large. Contact Admin");
+		} // need expection for wrong inputs
 		Plant plant = new Plant();
 		plant.setBloomTime(bloomTime);
 		plant.getBotanicalName();
@@ -138,56 +142,81 @@ public class PlantService {
 		plant.setPlantId(plantId);
 		plantRepository.save(plant);
 		return plant;
-		
+
 	}
-	
+
 	/**
-     * Add plant from database with its information onto the member's profile. 
-     * No changes done from the database. Will be assigned a new plantid
-	 * @param 
+	 * Add plant from database with its information onto the member's profile. No
+	 * changes done from the database. Will be assigned a new plantid
+	 * 
+	 * @param
 	 * @return plant
 	 */
-	public Plant addPlant(Integer plantId, String memberId){
-		if (plantId == null){
-			throw new IllegalArgumentException("Plant not found.");
-		}
-		if (memberId == null){
-			throw new IllegalArgumentException("Member not found.");
-		}
+	public Plant addPlant(Integer plantId, String memberId) {
+		/*
+		 * if (plantId == null){ throw new IllegalArgumentException("Plant not found.");
+		 * } if (memberId == null){ throw new
+		 * IllegalArgumentException("Member not found."); }
+		 */
 		Plant plant = plantRepository.findPlantByPlantId(plantId);
-		int leftLimit = 000101; //starting after the original plantid
+		/*
+		 * if (plant == null){ throw new IllegalArgumentException("Plant not found."); }
+		 */
+		int leftLimit = 000101; // starting after the original plantid
 		int rightLimit = 999999;
-	    int plantIdNew = leftLimit + (int) (Math.random() * (rightLimit - leftLimit));
+		int plantIdNew = leftLimit + (int) (Math.random() * (rightLimit - leftLimit));
+		/*
+		 * if (!isIdAvailable(plantIdNew)) { throw new
+		 * IllegalArgumentException("PlantId is Taken. Contact Admin"); }
+		 */
+		// Plant newPlant = plant;
 		Plant newPlant = plant;
-		newPlant.setPlantId(plantIdNew); //idk if this works
+		/*
+		 * Toxicity newPlantToxicity = plant.getToxicity(); String newPlantCommonName =
+		 * plant.getCommonName(); String newPlantBotanicalName =
+		 * plant.getBotanicalName(); String newPlantGivenName = plant.getGivenName();
+		 * Integer newPlantIcon= plant.getIcon(); SoilType newPlantSoil=
+		 * plant.getSoilType(); SunExposure newPlantSun = plant.getSunExposure();
+		 * WateringSchedule wateringSchedule = plant.getWateringRecommendation(); List
+		 * <BloomTime> newPlantBloom = plant.getBloomTime();
+		 * 
+		 * newPlant.setBloomTime(newPlantBloom);
+		 * newPlant.setGivenName(newPlantGivenName);
+		 * newPlant.setBotanicalName(newPlantBotanicalName);
+		 * newPlant.setCommonName(newPlantCommonName); newPlant.setPlantId(plantIdNew);
+		 * newPlant.setSunExposure(newPlantSun);
+		 * newPlant.setWateringRecommendation(wateringSchedule);
+		 * newPlant.setSoilType(newPlantSoil); newPlant.setToxicity(newPlantToxicity);
+		 */
+		newPlant.setPlantId(plantIdNew); // idk if this works
 		plantRepository.save(newPlant);
-	    Member member = memberRepository.findMemberByUsername(memberId);
-	    member.getPlant().add(newPlant);
-	    memberRepository.save(member);
-	    return newPlant;
-		
+		Member member = memberRepository.findMemberByUsername(memberId);
+		member.getPlant().add(newPlant);
+		memberRepository.save(member);
+		return newPlant;
+
 	}
-	
+
 	/**
-     * Change plant that was added  from database.
-     * Might be split into different Methods such as updateplanttoxicity since there
-     * are many exceptions for each.
+	 * Change plant that was added from database. Might be split into different
+	 * Methods such as updateplanttoxicity since there are many exceptions for each.
+	 * 
 	 * @param plantId
 	 * @return plant
 	 */
-	public Plant updatePlant(Integer plantId, String givenName, Integer icon, String botanicalName,
-			String commonName,SunExposure sunExposure,SoilType soilType,
-			Toxicity toxicity, List <BloomTime> bloomTime, WateringSchedule wateringRecommendation){
-		if (plantId == null){
+	public Plant updatePlant(Integer plantId, String givenName, Integer icon, String botanicalName, String commonName,
+			SunExposure sunExposure, SoilType soilType, Toxicity toxicity, List<BloomTime> bloomTime,
+			WateringSchedule wateringRecommendation) {
+		if (plantId == null) {
 			throw new IllegalArgumentException("Plant not found.");
 		}
-		if (givenName == null){
+		if (givenName == null) {
 			throw new IllegalArgumentException("Plant not found.");
 		}
 		Plant plant = plantRepository.findPlantByPlantId(plantId);
-		//updatePlantToxicity(plantId);
-		//updatePlantSun( plantId );
-		//updatePlantSoil( plantId );
+		// updatePlantToxicity(plantId);
+		// updatePlantSun( plantId );
+		// updatePlantSoil( plantId );
 		plant.setBloomTime(bloomTime);
 		plant.getBotanicalName();
 		plant.setCommonName(commonName);
@@ -202,103 +231,109 @@ public class PlantService {
 		plantRepository.save(plant);
 
 		return plant;
-		
+
 	}
-	
+
 	/**
-     * Delete Plant from Database and MemberProfile
+	 * Delete Plant from Database and MemberProfile
+	 * 
 	 * @param plantId
 	 * @return plant
 	 */
-	public Plant deletePlant(Integer plantId ){
-		if (plantId == null){
+	public Plant deletePlant(Integer plantId) {
+		if (plantId == null) {
 			throw new IllegalArgumentException("Plant not found.");
 		}
 		Plant plant = plantRepository.findPlantByPlantId(plantId);
 		if (plant == null) {
 			throw new IllegalArgumentException("Plant does not exist"); // @TODO loooooool
 		}
-		deletePlantForMember( plantId );
+		deletePlantForMember(plantId);
 		plantRepository.delete(plant);
 		return plant;
-		
-	}
-	
-	
-								/* HELPER METHODS*/
-			
-	
-	/*public Plant updatePlantToxicity(Integer plantId,List <Toxicity> toxicity ){
-		Plant plant = plantRepository.findPlantByPlantId(plantId);
-		plant.setToxicity(toxicity);
-		plantRepository.save(plant);
 
-		return plant;
-		
 	}
-	
-	public Plant updatePlantSoil(Integer plantId,List <SoilType> soilType){
-		Plant plant = plantRepository.findPlantByPlantId(plantId);
-		plant.setSoilType(soilType);
-		plantRepository.save(plant);
 
-		return plant;
-		
-	}
-	
-	public Plant updatePlantSun(Integer plantId,List <SunExposure> sunExposure){
-		Plant plant = plantRepository.findPlantByPlantId(plantId);
-		plant.setSunExposure(sunExposure);
-		plantRepository.save(plant);
+	/* HELPER METHODS */
 
-		return plant;
-		
-	}*/
-	
-	public Plant updateBloom(Integer plantId,List <BloomTime> bloomTime){
+	/*
+	 * public Plant updatePlantToxicity(Integer plantId,List <Toxicity> toxicity ){
+	 * Plant plant = plantRepository.findPlantByPlantId(plantId);
+	 * plant.setToxicity(toxicity); plantRepository.save(plant);
+	 * 
+	 * return plant;
+	 * 
+	 * }
+	 * 
+	 * public Plant updatePlantSoil(Integer plantId,List <SoilType> soilType){ Plant
+	 * plant = plantRepository.findPlantByPlantId(plantId);
+	 * plant.setSoilType(soilType); plantRepository.save(plant);
+	 * 
+	 * return plant;
+	 * 
+	 * }
+	 * 
+	 * public Plant updatePlantSun(Integer plantId,List <SunExposure> sunExposure){
+	 * Plant plant = plantRepository.findPlantByPlantId(plantId);
+	 * plant.setSunExposure(sunExposure); plantRepository.save(plant);
+	 * 
+	 * return plant;
+	 * 
+	 * }
+	 */
+
+	public Plant updateBloom(Integer plantId, List<BloomTime> bloomTime) {
 		Plant plant = plantRepository.findPlantByPlantId(plantId);
 		plant.setBloomTime(bloomTime);
 		plantRepository.save(plant);
 
 		return plant;
-		
+
 	}
-	
-	public Member getMemberbyPlantId(Integer plantId ){
-		
+
+	public Member getMemberbyPlantId(Integer plantId) {
+
 		Plant plant = plantRepository.findPlantByPlantId(plantId);
 		List<Member> members = toList(memberRepository.findAll());
-		for (Member member:members) {
-			if (member.getPlant() ==plant ) {
+		for (Member member : members) {
+			if (member.getPlant() == plant) {
 				return member;
 			}
 		}
 		throw new IllegalArgumentException("Member not found.");
 	}
-	
-	public Plant deletePlantForMember(Integer plantId ){
-		
+
+	public boolean isIdAvailable(Integer plantId) {
+
+		Plant plant = plantRepository.findPlantByPlantId(plantId);
+		if (plant != null) {
+			return false;
+		} else
+			return true;
+	}
+
+	public Plant deletePlantForMember(Integer plantId) {
+
 		Plant plant = plantRepository.findPlantByPlantId(plantId);
 		List<Member> members = toList(memberRepository.findAll());
-		int counter =0;
-		for (Member member:members) {
-			counter ++;
-			if (member.getPlant() ==plant ) {
+		int counter = 0;
+		for (Member member : members) {
+			counter++;
+			if (member.getPlant() == plant) {
 				member.getPlant().remove(counter);
 			}
 		}
 		throw new IllegalArgumentException("Member not found.");
 	}
-	
-	
-	
+
 	/**
-     * Helper method that converts iterable to list
+	 * Helper method that converts iterable to list
+	 * 
 	 * @param <T>
 	 * @param iterable
 	 * @return
 	 */
-	private <T> List<T> toList(Iterable<T> iterable){
+	private <T> List<T> toList(Iterable<T> iterable) {
 		List<T> resultList = new ArrayList<T>();
 		for (T t : iterable) {
 			resultList.add(t);
--- PlantCareApplication-Backend/src/test/java/ca/plantcare/service/TestPlantService.java
+++ PlantCareApplication-Backend/src/test/java/ca/plantcare/service/TestPlantService.java
@@ -9,6 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.lenient;
 
 import java.sql.Date;
@@ -34,22 +35,32 @@ import ca.plantcare.models.Plant.Toxicity;
 
 @ExtendWith(MockitoExtension.class)
 public class TestPlantService {
-	
-	//Plant myPlant = new Plant();
-	//Member myMember = new Member();
-	Integer PLANTID = 1111;
+
+	// Plant myPlant = new Plant();
+	// Member myMember = new Member();
+	Integer PLANTID = 7;
 	String BOTANICALNAME = "Botanical";
 	String GIVENNAME = "MyPlant";
 	String COMMONNAME = "Common";
-	//List<BloomTime> Bloomtime =  EnumSet.of( Bloomtime.);
-	BloomTime BLOOMTIME= BloomTime.Fall;
-    List <BloomTime> bloomiTime = new ArrayList<>();
+	// List<BloomTime> Bloomtime = EnumSet.of( Bloomtime.);
+	BloomTime BLOOMTIME = BloomTime.Fall;
+	List<BloomTime> bloomiTime = new ArrayList<>();
 	SoilType SOILTYPE = SoilType.Chalky;
 	SunExposure SUNEXPOSURE = SunExposure.FullSun;
 	Toxicity TOXICITY = Toxicity.MajorToxicity;
 	WateringSchedule wateringSchedule;
-	
-	
+
+	private static final String USERNAME1 = "Username1";
+	private static final String NAME1 = "Rajaa Bk";
+
+	private static final String GIVENNAME1 = "Elia";
+	private static final String BOTANICALNAME1 = "Sunflower";
+	private static final String COMMONNAME1 = "Sunflower";
+	private static final Integer PLANTID1 = 123;
+
+	private static final String USERNAME2 = "Username2";
+	private static final String NAME2 = "Catherine Carbon";
+
 	@Mock
 	private MemberRepository memberRepository;
 	@Mock
@@ -59,85 +70,112 @@ public class TestPlantService {
 
 	@InjectMocks
 	private PlantService plantService;
-	
+
 	@BeforeEach
-	public void setMockOutput() { 
-		
-		 lenient().when(plantRepository.findPlantByPlantId(anyInt())).thenAnswer((InvocationOnMock invocation) -> {
-	        	if (invocation.getArgument(0).equals(PLANTID)) {
-		        	Plant plant = new Plant();
-		        	Member member = new Member();
-		        bloomiTime.add(BLOOMTIME);
-		           plant.setBloomTime(bloomiTime);
-		           plant.setBotanicalName(BOTANICALNAME);
-		           plant.setCommonName(COMMONNAME);
-		           plant.setIcon(PLANTID);
-		           plant.setSoilType(SOILTYPE);
-		           plant.setSunExposure(SUNEXPOSURE);
-		           plant.setToxicity(TOXICITY);
-		           plant.setWateringRecommendation(wateringSchedule);
-		            return plant;
-	        	}
-	        	else {return null;
-	        	}
-	        	});
-		 Answer<?> returnParameterAsAnswer = (InvocationOnMock invocation) -> {
-				return invocation.getArgument(0);
-			};
-			lenient().when(memberRepository.save(any(Member.class))).thenAnswer(returnParameterAsAnswer);
-			lenient().when(plantRepository.save(any(Plant.class))).thenAnswer(returnParameterAsAnswer);
+	public void setMockOutput() {
+
+		lenient().when(plantRepository.findPlantByPlantId(anyInt())).thenAnswer((InvocationOnMock invocation) -> {
+			if (invocation.getArgument(0).equals(PLANTID)) {
+				Plant plant = new Plant();
+				Member member = new Member();
+				bloomiTime.add(BLOOMTIME);
+				plant.setBloomTime(bloomiTime);
+				plant.setBotanicalName(BOTANICALNAME);
+				plant.setCommonName(COMMONNAME);
+				plant.setIcon(PLANTID);
+				plant.setSoilType(SOILTYPE);
+				plant.setSunExposure(SUNEXPOSURE);
+				plant.setToxicity(TOXICITY);
+				plant.setWateringRecommendation(wateringSchedule);
+				return plant;
+			} else {
+				return null;
 			}
-	
+		});
+		lenient().when(memberRepository.findMemberByUsername(anyString())).thenAnswer((InvocationOnMock invocation) -> {
+			if (invocation.getArgument(0).equals(USERNAME1)) {
+				Member user = new Member();
+				user.setUsername(USERNAME1);
+				user.setName(NAME1);
+				List<Plant> plants = new ArrayList<Plant>();
+				Plant plant1 = new Plant();
+				plant1.setGivenName(GIVENNAME1);
+				plant1.setCommonName(COMMONNAME1);
+				plant1.setBotanicalName(BOTANICALNAME1);
+				plant1.setPlantId(PLANTID1);
+				plants.add(plant1);
+				user.setPlant(plants);
+				user.setNumberOfPlants(plants.size());
+				return user;
+			} else if (invocation.getArgument(0).equals(USERNAME2)) {
+				Member user = new Member();
+				user.setUsername(USERNAME2);
+				user.setName(NAME2);
+				return user;
+			} else {
+				return null;
+			}
+		});
+		Answer<?> returnParameterAsAnswer = (InvocationOnMock invocation) -> {
+			return invocation.getArgument(0);
+		};
+		lenient().when(memberRepository.save(any(Member.class))).thenAnswer(returnParameterAsAnswer);
+		lenient().when(plantRepository.save(any(Plant.class))).thenAnswer(returnParameterAsAnswer);
+	}
+
 	@Test
 	public void testCreatePlantPass() {
 		Plant plant = null;
 		try {
-			Integer myPlantId = 1111;
+			Integer myPlantId = 5;
 			String myBotanicalName = "Botanical";
 			String myGivenName = "MyPlant";
 			String myCommonName = "Common";
-			//List<BloomTime> Bloomtime =  EnumSet.of( Bloomtime.);
-			BloomTime myBloomTime= BloomTime.Fall;
-		    List <BloomTime> myBloomiTime = new ArrayList<>();
-		    myBloomiTime.add(myBloomTime);
+			// List<BloomTime> Bloomtime = EnumSet.of( Bloomtime.);
+			BloomTime myBloomTime = BloomTime.Fall;
+			List<BloomTime> myBloomiTime = new ArrayList<>();
+			myBloomiTime.add(myBloomTime);
 			SoilType mySoilType = SoilType.Chalky;
 			SunExposure mySun = SunExposure.FullSun;
 			Toxicity myToxic = Toxicity.MajorToxicity;
 			WateringSchedule myWateringSchedule = new WateringSchedule();
-			plant = plantService.createPlant(myPlantId, myGivenName, myBotanicalName, myCommonName, mySun, mySoilType, myToxic, myBloomiTime, myWateringSchedule);
+			plant = plantService.createPlant(myPlantId, myGivenName, myBotanicalName, myCommonName, mySun, mySoilType,
+					myToxic, myBloomiTime, myWateringSchedule);
 		} catch (IllegalArgumentException e) {
 			// Check that no error occurred
 			fail();
 		}
 		assertNotNull(plant);
 	}
+
 	@Test
 	public void testCreatePlantNoWateringSchedule() {
 		Plant plant = null;
 		String error = "";
 		try {
-			Integer myPlantId = 1111;
+			Integer myPlantId = 2;
 			String myBotanicalName = "Botanical";
 			String myGivenName = "MyPlant";
 			String myCommonName = "Common";
-			//List<BloomTime> Bloomtime =  EnumSet.of( Bloomtime.);
-			BloomTime myBloomTime= BloomTime.Fall;
-		    List <BloomTime> myBloomiTime = new ArrayList<>();
-		    myBloomiTime.add(myBloomTime);
+			// List<BloomTime> Bloomtime = EnumSet.of( Bloomtime.);
+			BloomTime myBloomTime = BloomTime.Fall;
+			List<BloomTime> myBloomiTime = new ArrayList<>();
+			myBloomiTime.add(myBloomTime);
 			SoilType mySoilType = SoilType.Chalky;
 			SunExposure mySun = SunExposure.FullSun;
 			Toxicity myToxic = Toxicity.MajorToxicity;
 			WateringSchedule myWateringSchedule = null;
-			plant = plantService.createPlant(myPlantId, myGivenName, myBotanicalName, myCommonName, mySun, mySoilType, myToxic, myBloomiTime, myWateringSchedule);
+			plant = plantService.createPlant(myPlantId, myGivenName, myBotanicalName, myCommonName, mySun, mySoilType,
+					myToxic, myBloomiTime, myWateringSchedule);
 		} catch (Exception e) {
 			// Check that no error occurred
-			error=e.getMessage();
+			error = e.getMessage();
 		}
 		assertNull(plant);
 		assertNotNull(error);
 
 	}
-	
+
 	@Test
 	public void testCreatePlantNoBloomTime() {
 		Plant plant = null;
@@ -147,23 +185,121 @@ public class TestPlantService {
 			String myBotanicalName = "Botanical";
 			String myGivenName = "MyPlant";
 			String myCommonName = "Common";
-			//List<BloomTime> Bloomtime =  EnumSet.of( Bloomtime.);
-			BloomTime myBloomTime= BloomTime.Fall;
-		    List <BloomTime> myBloomiTime = new ArrayList<>();
-		    //myBloomiTime.add(myBloomTime);
+			// List<BloomTime> Bloomtime = EnumSet.of( Bloomtime.);
+			BloomTime myBloomTime = BloomTime.Fall;
+			List<BloomTime> myBloomiTime = new ArrayList<>();
+			// myBloomiTime.add(myBloomTime);
 			SoilType mySoilType = SoilType.Chalky;
 			SunExposure mySun = SunExposure.FullSun;
 			Toxicity myToxic = Toxicity.MajorToxicity;
 			WateringSchedule myWateringSchedule = null;
-			plant = plantService.createPlant(myPlantId, myGivenName, myBotanicalName, myCommonName, mySun, mySoilType, myToxic, myBloomiTime, myWateringSchedule);
+			plant = plantService.createPlant(myPlantId, myGivenName, myBotanicalName, myCommonName, mySun, mySoilType,
+					myToxic, myBloomiTime, myWateringSchedule);
+
 		} catch (Exception e) {
 			// Check that no error occurred
-			error=e.getMessage();
+			error = e.getMessage();
 		}
 		assertNull(plant);
 		assertNotNull(error);
 
 	}
-	
+
+	@Test
+	public void testCreatePlantWrongId() {
+		Plant plant = null;
+		String error = "";
+		try {
+			Integer myPlantId = 1111;
+			String myBotanicalName = "Botanical";
+			String myGivenName = "MyPlant";
+			String myCommonName = "Common";
+			// List<BloomTime> Bloomtime = EnumSet.of( Bloomtime.);
+			BloomTime myBloomTime = BloomTime.Fall;
+			List<BloomTime> myBloomiTime = new ArrayList<>();
+			// myBloomiTime.add(myBloomTime);
+			SoilType mySoilType = SoilType.Chalky;
+			SunExposure mySun = SunExposure.FullSun;
+			Toxicity myToxic = Toxicity.MajorToxicity;
+			WateringSchedule myWateringSchedule = null;
+			plant = plantService.createPlant(myPlantId, myGivenName, myBotanicalName, myCommonName, mySun, mySoilType,
+					myToxic, myBloomiTime, myWateringSchedule);
+		} catch (Exception e) {
+			// Check that no error occurred
+			error = e.getMessage();
+		}
+		assertNull(plant);
+		assertNotNull(error);
+
+	}
+
+	@Test
+	public void updateCreatePlantPass() {
+		Plant plant = null;
+		try {
+
+			String myBotanicalName = "Botanical";
+			String myGivenName = "MyPlant";
+			Integer icon = 3;
+			String myCommonName = "Common";
+			// List<BloomTime> Bloomtime = EnumSet.of( Bloomtime.);
+			BloomTime myBloomTime = BloomTime.Fall;
+			List<BloomTime> myBloomiTime = new ArrayList<>();
+			myBloomiTime.add(myBloomTime);
+			SoilType mySoilType = SoilType.Chalky;
+			SunExposure mySun = SunExposure.FullSun;
+			Toxicity myToxic = Toxicity.MajorToxicity;
+			WateringSchedule myWateringSchedule = new WateringSchedule();
+			plant = plantService.updatePlant(PLANTID, myGivenName, icon, myBotanicalName, myCommonName, mySun,
+					mySoilType, myToxic, myBloomiTime, myWateringSchedule);
+		} catch (IllegalArgumentException e) {
+			// Check that no error occurred
+			fail();
+		}
+		assertNotNull(plant);
+	}
+
+	@Test
+	public void addPlantSuccess() {
+		Plant plant2 = null;
+
+		String error = "";
+
+		String memberId = "1";
+
+		try {
+			// plant = plantService.createPlant(myPlantId, myGivenName, myBotanicalName,
+			// myCommonName, mySun, mySoilType, myToxic, myBloomiTime, myWateringSchedule);
+			// plantRepository.save(plant);
+
+			plant2 = plantService.addPlant(PLANTID, USERNAME1);
+		} catch (Exception e) {
+
+			error = e.getMessage();
+		}
+		// assertNotNull(plant);
+
+		assertNotNull(plant2);
+
+	}
+
+	@Test
+	public void addPlantWrongId() {
+
+		Plant plant2 = null;
+		String error = "";
+		String memberId = "1";
+		Integer myPlantId = 2;
+		try {
+
+			plant2 = plantService.addPlant(myPlantId, memberId);
+		} catch (Exception e) {
+			// Check that no error occurred
+			error = e.getMessage();
+		}
+		assertNull(plant2);
+		assertNotNull(error);
+
+	}
 
 }
