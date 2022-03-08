a4ee65f43de2a8512293dc178587276dbdcd86e2
Catherine Caron <catherine.caron2@mail.mcgill.ca>
2022-03-07 19:08:18 -0500

    trying stuff

---
+++ PlantCareApplication-Android/app/src/main/java/ca/plantcare/HomePage.java
@@ -0,0 +1,14 @@
+package ca.plantcare;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
