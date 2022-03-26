35de839b413c96be2abe025b756cc5563376012e
rajaabk <rajaa.bk0@gmail.com>
2022-03-26 15:08:37 -0400

    Issues with Controller

--- PlantCareApplication-Backend/src/main/java/ca/plantcare/models/Plant.java
+++ PlantCareApplication-Backend/src/main/java/ca/plantcare/models/Plant.java
@@ -8,9 +8,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
-
+@Table (name = "plant")
 @Entity
 public class Plant {
 
@@ -204,7 +206,7 @@ public class Plant {
 	/**
 	 * @return the member
 	 */
-	@OneToOne(cascade = CascadeType.ALL)
+	@OneToMany(cascade = CascadeType.ALL)
 	public Member getMember() {
 		return member;
 	}
