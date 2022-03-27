0ff074ecd5e3f36d046c1351a0b60573111143b2
Catherine Caron <catherine.caron2@mail.mcgill.ca>
2022-03-26 23:28:39 -0400

    reminders and schedule controllers done

--- PlantCareApplication-Backend/src/main/java/ca/plantcare/controller/ReminderController.java
+++ PlantCareApplication-Backend/src/main/java/ca/plantcare/controller/ReminderController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.Date;
+import java.sql.Time;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -29,13 +31,18 @@ public class ReminderController {
 
     @Autowired
 	private ReminderService reminderService;
+
+    @Autowired
+	private WateringScheduleService wateringScheduleService;
     
 	private static final String BASE_URL = "/reminder";
+
+
 	/**
 	 * Return the reminder with specified id
 	 * 
 	 * @param reminderId
-	 * @return Member Dto
+	 * @return Reminder Dto
 	 */
 	@GetMapping(value = { BASE_URL+ "/getReminderById/{reminderId}", BASE_URL+ "/getReminderById/{reminderId}/" })
 	public ReminderDto getReminderById(@PathVariable("reminderId") Integer reminderId) {
@@ -43,11 +50,92 @@ public class ReminderController {
         return ReminderDto.convertToDto(reminder);
 	}
 
-    // createReminder(Integer scheduleId, String message, Time time, Date date)
-    // getReminderById(Integer reminderId)
-    // updateReminder(Integer reminderId, String newMessage, Time newTime, Date newDate)
-    // deleteReminder(Integer reminderId)
-    // getReminderByWateringSchedule(Integer scheduleId)
+
+    /**
+	 * Create a Reminder Dto with provided parameters
+	 * 
+	 * @return Reminder  Dto
+	 */
+	@PostMapping(value = { BASE_URL+"/createReminder", BASE_URL+"/createReminder/" })
+	public ResponseEntity<?>createReminder(
+			@RequestParam("scheduleId") Integer scheduleId,
+			@RequestParam("message") String message,
+			@RequestParam("time") Time time,
+			@RequestParam("date") Date date)
+			{
+		
+		try {
+			Reminder reminder = reminderService.createReminder(scheduleId, message, time, date);
+			return httpSuccess(ReminderDto.convertToDto(reminder));
+
+		}
+		catch(Exception e){
+			return httpFailure("Error: " + e.getMessage());
+		}
+	}
+	
+    /**
+	 * Update a Reminder 
+     * Pass old values if not changing
+	 * 
+	 * @return Reminder Dto updated
+	 */
+	@PutMapping(value = { BASE_URL+"/updateReminder", BASE_URL+"/updateReminder/" })
+	public ResponseEntity<?>updateReminder(
+			@RequestParam("reminderId") Integer reminderId,
+			@RequestParam("newMessage") String newMessage,
+			@RequestParam("newTime") Time newTime,
+			@RequestParam("newDate") Date newDate)
+			{
+		
+		try {
+			Reminder reminder = reminderService.updateReminder(reminderId, newMessage, newTime, newDate);
+			return httpSuccess(ReminderDto.convertToDto(reminder));
+
+		}
+		catch(Exception e){
+			return httpFailure("Error: " + e.getMessage());
+		}
+	}
+
+   
+    /**
+	 * Delete a Reminder 
+	 * 
+	 * @return Reminder Dto deleted
+	 */
+	@PutMapping(value = { BASE_URL+"/deleteReminder", BASE_URL+"/deleteReminder/" })
+	public ResponseEntity<?>deleteReminder(
+			@RequestParam("reminderId") Integer reminderId)
+			{
+		
+		try {
+			Reminder reminder = reminderService.deleteReminder(reminderId);
+			return httpSuccess(ReminderDto.convertToDto(reminder));
+
+		}
+		catch(Exception e){
+			return httpFailure("Error: " + e.getMessage());
+		}
+	}
+
+
+    /**
+	 * Return the reminder associated to the watering schedule
+	 * 
+	 * @param reminderId
+	 * @return Reminder Dto
+	 */
+	@GetMapping(value = { BASE_URL+ "/getReminderByWateringSchedule/{scheduleId}", BASE_URL+ "/getReminderByWateringSchedule/{scheduleId}/" })
+	public  ResponseEntity<?> getReminderByWateringSchedule(@PathVariable("scheduleId") Integer scheduleId) {
+        List<ReminderDto> reminders = null;
+		try {
+		    reminders = reminderService.getReminderByWateringSchedule(scheduleId).stream().map(reminder -> ReminderDto.convertToDto(reminder)).collect(Collectors.toList());
+        } catch (Exception e) {
+			return httpFailureMessage(e.getMessage());
+		}
+		return httpSuccess(reminders);
+	}
 
 
 
--- PlantCareApplication-Backend/src/main/java/ca/plantcare/controller/WateringScheduleController.java
+++ PlantCareApplication-Backend/src/main/java/ca/plantcare/controller/WateringScheduleController.java
@@ -3,10 +3,15 @@ package ca.plantcare.controller;
 import static ca.plantcare.extra.HttpUtil.httpFailure;
 import static ca.plantcare.extra.HttpUtil.httpSuccess;
 
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,7 +37,13 @@ public class WateringScheduleController {
 	@Autowired
 	private WateringScheduleService wateringScheduleService;
 	
-	@PostMapping(value = { BASE_URL + "/create", BASE_URL + "/create/" })
+	/**
+	 * Create watering schedule for a plant
+	 * @param plantId
+	 * @param hoursBetweenWatering
+	 * @return
+	 */
+	@PostMapping(value = { BASE_URL + "/createWateringSchedule", BASE_URL + "/createWateringSchedule/" })
 	public ResponseEntity<?> createWateringSchedule(@RequestParam("plantId") Integer plantId, 
 			@RequestParam("hoursBetweenWatering") Integer hoursBetweenWatering)  {
 		try {
@@ -43,6 +54,64 @@ public class WateringScheduleController {
 		catch(Exception e){
 			return httpFailure("Error: " + e.getMessage());
 		}
-	
-	
-}}
+	}
+
+	/**
+	 * Return the schedule with specified id
+	 * 
+	 * @param scheduleId
+	 * @return Reminder Dto
+	 */
+	@GetMapping(value = { BASE_URL+ "/getWateringScheduleById/{scheduleId}", BASE_URL+ "/getWateringScheduleById/{scheduleId}/" })
+	public WateringScheduleDto getWateringScheduleById(@PathVariable("scheduleId") Integer scheduleId) {
+		WateringSchedule schedule = wateringScheduleService.getWateringScheduleById(scheduleId);
+        return WateringScheduleDto.converToDto(schedule);
+	}
+
+	/**
+	 * Update hours between watering
+	 * @param scheduleId
+	 * @param newHoursBetweenWatering
+	 * @return
+	 */
+	@PutMapping(value = { BASE_URL + "/updateWateringSchedule", BASE_URL + "/updateWateringSchedule/" })
+	public ResponseEntity<?> updateWateringSchedule(@RequestParam("scheduleId") Integer scheduleId, 
+			@RequestParam("newHoursBetweenWatering") Integer newHoursBetweenWatering)  {
+		try {
+			WateringSchedule wateringSchedule = wateringScheduleService.updateWateringSchedule(scheduleId, newHoursBetweenWatering);
+					return httpSuccess(WateringScheduleDto.converToDto(wateringSchedule));
+		}
+		catch(Exception e){
+			return httpFailure("Error: " + e.getMessage());
+		}
+	}
+
+	/**
+	 * Delete schedule with Id
+	 * @param scheduleId
+	 * @return
+	 */
+	@PutMapping(value = { BASE_URL + "/deleteWateringSchedule", BASE_URL + "/deleteWateringSchedule/" })
+	public ResponseEntity<?> deleteWateringSchedule(@RequestParam("scheduleId") Integer scheduleId)  {
+		try {
+			WateringSchedule wateringSchedule = wateringScheduleService.deleteWateringSchedule(scheduleId);
+					return httpSuccess(WateringScheduleDto.converToDto(wateringSchedule));
+		}
+		catch(Exception e){
+			return httpFailure("Error: " + e.getMessage());
+		}
+	}
+
+	/**
+	 * Find the watering schedule for a plant with Id
+	 * @param plantId
+	 * @return
+	 */
+	@GetMapping(value = { BASE_URL+ "/getWateringScheduleByPlant/{plantId}", BASE_URL+ "/getWateringScheduleByPlant/{plantId}/" })
+	public WateringScheduleDto getWateringScheduleByPlant(@PathVariable("plantId") Integer plantId) {
+		WateringSchedule schedule = wateringScheduleService.getWateringScheduleByPlant(plantId);
+        return WateringScheduleDto.converToDto(schedule);
+	}
+
+
+}
